e4c2242e3b35eaf1ae7a9ab7cf1dd78c81630e3c
Phan An Hòa <phananhoa25082007w@gmail.com>
2025-12-06 10:36:58 +0700

    FINAL UPDATE
    Update: DO NOT USE, it treats lowercase and uppercase letters interpedently

--- detectkytusieumoikoaithich.c
+++ detectkytusieumoikoaithich.c
@@ -2,28 +2,44 @@
 #include <string.h>
 
 int main() {
-    char str[100];
+    char str[1000];
     char kt;
     int demKyTu = 0;
 
+    // Nhap chuoi
     printf("Nhap chuoi bat ky: ");
-    fgets(str, sizeof(str), stdin); // nhập chuỗi
-    str[strcspn(str, "\n")] = '\0'; // loại bỏ ký tự xuống dòng
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("Loi khi nhap chuoi.\n");
+        return 1;
+    }
+    // Xoa ky tu newline neu co
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
 
+    // In lai chuoi (nhu terminal trong anh)
+    printf("%s\n\n", str);
+
+    // Nhap ky tu muon tim (bo qua khoang trang/newline truoc do)
     printf("Nhap ky tu muon tim trong chuoi: ");
-    scanf(" %c", &kt); // thêm khoảng trắng để bỏ qua ký tự thừa
+    if (scanf(" %c", &kt) != 1) {
+        printf("Loi khi nhap ky tu.\n");
+        return 1;
+    }
 
-    // Đếm số lần xuất hiện của kt trong str
-    for (int i = 0; i < strlen(str); i++) {
+    // Dem so lan xuat hien
+    for (size_t i = 0; i < strlen(str); i++) {
         if (str[i] == kt) {
             demKyTu++;
         }
     }
 
+    // Xuat ket qua
     if (demKyTu > 0) {
-        printf("Ky tu '%c' xuat hien %d lan trong chuoi \"%s\".\n", kt, demKyTu, str);
+        printf("%c xuat hien %d lan trong chuoi.\n", kt, demKyTu);
     } else {
-        printf("Khong tim thay ky tu '%c' trong chuoi \"%s\".\n", kt, str);
+        printf("Khong tim thay %c trong chuoi %s", kt, str);
     }
 
     return 0;
